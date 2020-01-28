e26c5733cfc240c0fcc882b9929641dee24f8b62
Angeline R <4ng133@gmail.com>
2020-01-28 17:12:22 +0000

    create chat app with react

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+function Contact(props) {
+    return (
+        <div className="Contact">
+            <img src={props.avatar} className="avatar" />
+            <div>
+                <h4 className="name">{props.name}</h4>
+                <div className="status">
+                    <div className={props.online ? "status-online" : "status-offline"} />
+                    <span className="status-text">
+                        {props.online ? "online" : "offline"}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
